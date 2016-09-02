459a2a922409cc9c3bad8682adc2a06d64f81246
Alexey Kosolapov <klealekle@gmail.com>
2016-09-02 11:49:18 +0300

    header start

--- src/style/var.js
+++ src/style/var.js
@@ -4,13 +4,12 @@ module.exports = {
     "$S": "300px", 
     "$SSM": "380px",
     "$SM": "550px", 
+    "$MH": "600px",
     "$toM": "767px",
     "$M": "768px", 
     "$XM": "991px", 
     "$L": "1200px", 
     "$XL": "1328px",
-  
-    "$MH": "600px",
   
     // Colors
     "$text-main": "#fff",
@@ -18,19 +17,15 @@ module.exports = {
     "$text-gray-2": "#6c6c6c",
     "$text-gray-3": "#8f8f8f",
 
-    "$link-blue": "#2faee7",
-    "$link-blue-active": "#2faee7",
-    "$link-light-blue": "#a6c6cb",
-    "$link-light-blue-active": "#fff",
+    "$link-color": "#2faee7",
+    "$link-color-active": "#2faee7",
     "$link-white": "#fff",
     "$link-white-active": "#a6c6cb",
     "$link-gray": "#b7b7b7",
-    "$link-gray-1": "#6c6c6c",
     "$link-gray-active": "#fff",
   
-    "$body-bg": "#2b2b2b",
-    "$bg-main": "#33393c",
-    "$bg-sub": "#252a2c",
+    "$body-color": "#fff",
+    "$bg-sub-section": "#252a2c",
   
     "$button-blue": "#037db5",
     "$button-blue-text": "#fff",
@@ -40,23 +35,19 @@ module.exports = {
     "$bg-dropdown": "#305c7b",
     "$bg-form": "#444950",
   
-    // bg images
-    "$section_bg_1": "section_bg_1.jpg",
-  
     "$green": "#82ac4e",
     "$red": "#ea6465",
     "$subgreen": "#e8f39a",
     "$blue": "#037db5",
-    "$overlay-blue": "rgba(3, 125, 181, 0.13)",
   
     //transitions
-    "$tease": "all .2s ease",
+    "$transition": "all .2s ease",
   
     //border-radius
     "$br": "2px",
+  
     //box-shadow
     "$bsh": "1px 1px 2px rgba(0, 0, 0, 0.3)",
     "$bsh2": "4px 4px 10px rgba(0, 0, 0, 0.4)"
   
-    // choosen
 };
